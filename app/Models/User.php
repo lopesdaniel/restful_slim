1e3edf39852e746623c40e6ff4c08d6ef2e355d0
Daniel Lopes <viana.daniel02@gmail.com>
2019-01-13 22:28:04 -0300

    Adicionando segurança à senha

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,4 +14,14 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 class User extends Eloquent
 {
     protected $fillable = ['name', 'email', 'password'];
+
+    protected $hidden = ['password'];
+
+    public function setPasswordAttribute($key, $value)
+    {
+        if(!empty($value)){
+            $this->attributes['password'] = password_hash($value, PASSWORD_DEFAULT);
+        }
+    }
+
 }
